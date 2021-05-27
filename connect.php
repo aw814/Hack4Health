c6f63ab4aefea445945bc2491515c1e7b4038209
Michael Borromeo <michael@bamdigital.com>
2021-05-26 22:28:15 -0700

    api key and DB details hidden using simple approach

--- connect.php
+++ connect.php
@@ -1,13 +1,11 @@
 <?php
-    require_once("config.php");
-
-    $dbhost = $CONFIG_SQL_DB_HOST;
-    $username = $CONFIG_SQL_DB_USERNAME;
-    $password = $CONFIG_SQL_DB_PASSWORD;
-    $dbname = $CONFIG_SQL_DB_NAME;
-
     function OpenCon()
     {
+        require("config.php");
+        $dbhost = $CONFIG_SQL_DB_HOST;
+        $username = $CONFIG_SQL_DB_USERNAME;
+        $password = $CONFIG_SQL_DB_PASSWORD;
+        $dbname = $CONFIG_SQL_DB_NAME;
         $conn = new mysqli($dbhost, $username, $password, $dbname) or die ("Connect failed: %s\n". $conn->error);
         return $conn;
     }
